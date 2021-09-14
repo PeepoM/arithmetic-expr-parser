1993f1df59c81f63800ed3b5e08ac63a07d8d161
PeepoM <peter.marks@outlook.sk>
2021-09-14 03:12:47 +0200

    implemented an instance of Functor for the parser

--- Parser.hs
+++ Parser.hs
@@ -1,3 +1,9 @@
 module Parser where
 
 newtype Parser a = Parser {runParser :: String -> Maybe (a, String)}
+
+first :: (a -> c) -> (a, b) -> (c, b)
+first f (a, b) = (f a, b)
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ fmap (first f) . p
