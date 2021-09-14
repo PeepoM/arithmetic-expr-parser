7e2cfc9943784d9e4dbaa036a99c86ef4f75ddf6
PeepoM <peter.marks@outlook.sk>
2021-09-15 00:58:50 +0200

    added a couple of primitive parsers

--- Parser.hs
+++ Parser.hs
@@ -29,3 +29,44 @@ instance Monad Parser where
 instance Alternative Parser where
   empty = Parser $ const Nothing
   Parser p1 <|> Parser p2 = Parser $ (<|>) <$> p1 <*> p2
+
+-- Primitive parsers
+
+item :: Parser Char
+item = Parser f
+  where
+    f [] = Nothing
+    f (x : xs) = Just (x, xs)
+
+-- Parse a character that satisfies a given predicate
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p =
+  do
+    i <- item
+    if p i
+      then return i
+      else empty
+
+-- Parse a character of choice
+char :: Char -> Parser Char
+char c = satisfy (== c)
+
+-- Parse an integer
+int :: Parser Int
+int = char '-' *> (negate <$> posInt) <|> posInt
+
+-- Parse a positive integer
+posInt :: Parser Int
+posInt = Parser f
+  where
+    f xs
+      | null num = Nothing
+      | otherwise = Just (read num, rest)
+      where
+        (num, rest) = span isDigit xs
+
+-- Parse zero or more spaces
+spaces :: Parser String
+spaces = many $ satisfy isSpace
+
+--------------------
